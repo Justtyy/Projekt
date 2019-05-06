b818872140fec72f7cf90a9a998e86a39efde545
Ania <Ania@192.168.1.20>
2019-05-07 00:01:17 +0200

    Zmienna prędkość planety

--- Project Kepler's Laws/src/pl/edu/pw/fizyka/pojava/KierznowskaKurek/SimulationField.java
+++ Project Kepler's Laws/src/pl/edu/pw/fizyka/pojava/KierznowskaKurek/SimulationField.java
@@ -26,10 +26,9 @@ public class SimulationField extends JPanel implements Runnable{
 			g.fillOval(intPlanetX, intPlanetY, 10, 10);
 	       
 	  }
-	@Override
-	public void run() {
-		for(fi = 0; fi < 1000; fi+=0.1) {
-			planetX = ((orbit.semimajorAxis)*(Math.cos(fi))+(orbit.semiminorAxisEndX));
+	  
+	  public void planetMove() {
+		  planetX = ((orbit.semimajorAxis)*(Math.cos(fi))+(orbit.semiminorAxisEndX));
 			intPlanetX = (int)planetX;
 			
 			planetY = ((orbit.semiminorAxis)*(Math.sin(fi))+(orbit.semimajorAxisStartX)-5);
@@ -41,9 +40,36 @@ public class SimulationField extends JPanel implements Runnable{
 			}
 
 			repaint(); 
-		}
+	  }
+	@Override
+	public void run() {
+		for(int ii = 0; ii < 1000; ii++) {
 		
+			for(fi = 0; fi < 1.046; fi+=0.1) {
+				planetMove();
+			}
+		
+			for(fi = 1.046; fi < 2.093; fi+=0.08) {
+				planetMove();
+			}
+		
+			for(fi = 2.093; fi < 3.139; fi+=0.06) {
+				planetMove();
+			}
+		
+			for(fi = 3.139; fi < 4.185; fi+=0.06) {
+				planetMove();
+			}
+		
+			for(fi = 4.185; fi < 5.23; fi+=0.08) {
+				planetMove();
+			}
+		
+			for(fi = 5.23; fi < 6.28; fi+=0.1) {
+				planetMove();
+			}
+		
+		}
+	
 	}
-	
-	
 }
